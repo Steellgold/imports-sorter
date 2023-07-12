944a121becc7ada504f4d47363924d29565db18f
Gaëtan H <steellgold0@gmail.com>
2023-07-12 17:03:17 +0200

    chore: add .gitignore file
    chore: add .npmrc file
    
    chore: add .vscode/extensions.json file
    
    chore: add .vscode/launch.json file
    
    chore: add .vscode/settings.json file
    
    chore: add .vscode/tasks.json file
    
    chore: add .vscodeignore file
    
    chore: add CHANGELOG.md file
    
    chore: add README.md file
    
    chore: add icon.png file
    
    chore: add package.json file
    
    feat: add src/extension.ts file
    
    chore: add tsconfig.json file
    
    feat(vsc-extension-quickstart.md): add VS Code extension quickstart guide
    
    This commit adds the VS Code extension quickstart guide to the repository. The guide provides an overview of the folder structure and files in the extension, as well as instructions on how to get up and running, make changes, explore the API, run tests, and go further with extension development. It also includes links to additional resources for following UX guidelines, bundling and publishing extensions, and setting up continuous integration.

---
+++ src/extension.ts
@@ -0,0 +1,82 @@
+import * as vscode from "vscode";
+
+export const activate = (context: vscode.ExtensionContext): void => {
+  const disposable = vscode.commands.registerCommand("extension.sortImports", () => {
+    updateImports(false);
+  });
+
+  vscode.workspace.onWillSaveTextDocument(() => {
+    updateImports(false);
+  });
+
+  context.subscriptions.push(disposable);
+};
+
+const updateImports = (ascending: boolean): void => {
+  const editor = vscode.window.activeTextEditor;
+  if (editor) {
+    const document = editor.document;
+    const text = document.getText();
+
+    const lines = text.split("\n");
+    const imports = [];
+    const nonImports = [];
+
+    const importStartKeywords: { [key: string]: string[] } = {
+      javascript: ["import", "export", "require", "module.exports", "export default"],
+      python: ["import"],
+      java: ["import", "package"],
+      csharp: ["using"],
+      cpp: ["#include"],
+      ruby: ["require"],
+      php: ["require", "include", "namespace", "use"],
+      swift: ["import"],
+      go: ["import"],
+      kotlin: ["import"],
+      rust: ["use", "extern crate"],
+      scala: ["import"],
+      groovy: ["import"],
+      perl: ["use"],
+      lua: ["require"],
+      r: ["library"],
+      matlab: ["import"],
+      haskell: ["import"],
+      shell: ["source", "."]
+    };
+
+    for (const line of lines) {
+      const languageId = document.languageId.toLowerCase();
+
+      if (!languageId) {
+        continue;
+      }
+
+      const startKeywords = importStartKeywords[languageId];
+
+      if (startKeywords && Array.isArray(startKeywords) && startKeywords.some((keyword: string) => line.trim().startsWith(keyword))) {
+        imports.push(line);
+      } else {
+        nonImports.push(line);
+      }
+    }
+
+    if (ascending) {
+      imports.sort((a, b) => a.length - b.length);
+    } else {
+      imports.sort((a, b) => b.length - a.length);
+    }
+
+    const updatedText = [...imports, ...nonImports].join("\n");
+
+    void editor.edit(editBuilder => {
+      const firstLine = document.lineAt(0);
+      const lastLine = document.lineAt(document.lineCount - 1);
+      const textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
+      editBuilder.replace(textRange, updatedText);
+    });
+  }
+};
+
+export const deactivate = (): void => {
+  console.log("Your extension \"imports-sorter\" is now deactivated!");
+};
